b268603f441e862273c41a8d824d64590378373e
ajanders <ajanders@uw.edu>
2017-05-22 11:24:24 -0700

    Saved features of participant 1 for 6 activities for participant 1

--- HandFeaturization.m
+++ HandFeaturization.m
@@ -1,13 +1,14 @@
 % Featurization of RAW EMG data
 
 clear all; close all; clc;
-
+tic
 %% Section 1: read in data, slice into 150 ms cube
 dt = 1/1200;
 time = 0:dt:3;
 n = int16(.150/dt); %row length of 150 ms sample
 cut = int16(.512/dt);
-activity = 'crouch_';
+activity = 'fastWalk_';
+participant = 1;
 
 %data_out = zeros(n,16,16*9);
 ind2 = 1;
@@ -77,20 +78,20 @@ for k = 100:150
     end
 end
 
-if strcmp(activity,'crouch_')  
-    data_crouch = data_out;
-elseif strcmp(activity,'slowWalk_')  
-    data_slowwalk = data_out;
-elseif strcmp(activity,'sitting_')  
-    data_sitting = data_out;
-elseif strcmp(activity,'fastWalk_')  
-    data_fastWalk = data_out;
-elseif strcmp(activity,'standing_')  
-    data_standing = data_out;
-end
+% if strcmp(activity,'crouch_')  
+%       = data_out;
+% elseif strcmp(activity,'slowWalk_')  
+%     data_slowwalk = data_out;
+% elseif strcmp(activity,'sitting_')  
+%     data_sitting = data_out;
+% elseif strcmp(activity,'fastWalk_')  
+%     data_fastWalk = data_out;
+% elseif strcmp(activity,'standing_')  
+%     data_standing = data_out;
+% end
 
 [signal, sensor, trial] = size(data_out);
-clearvars data_out
+% clearvars data_out
     
 %% Section 2: calculate MAV, Variance, Nzero, etc
 
@@ -101,37 +102,71 @@ for i = 1:trial
     
     for n = 1:sensor
     % Mean Average Value
-    data_out(1,n,i) = mean(data_crouch(:,n,i));
-    MAV(n,i) = mean(data_crouch(:,n,i));
+    data_feat(1,n,i) = mean(data_out(:,n,i));
 
     % Number of zero crosssings
-    data_out(2,n,i) = zcd(data_crouch(:,n,i));
-    Ncross(n,i) = zcd(data_crouch(:,n,i));
+    data_feat(2,n,i) = zcd(data_out(:,n,i));
 
     % Variance
-    data_out(3,n,i) = var(data_crouch(:,n,i));
-    variance(n,i) = std(data_crouch(:,n,i));
+    data_feat(3,n,i) = var(data_out(:,n,i));
 
     % Number of slope sign changes
-    slope = diff(sign(diff(data_crouch(:,n,i))))~=0;
-    slope = slope(slope~=0);
-    slope = length(slope);
-    Nslope(n,i) = slope;
-    [a,b] = findpeaks(data_crouch(:,n,i));
-    [c,d] = findpeaks(-data_crouch(:,n,i));
-    data_out(4,n,i) = length(a)+length(b);
+%     slope = diff(sign(diff(data_out(:,n,i))))~=0;
+%     slope = slope(slope~=0);
+%     slope = length(slope);
+%     Nslope(n,i) = slope;
+    [a,b] = findpeaks(data_out(:,n,i));
+    [c,d] = findpeaks(-data_out(:,n,i));
+    data_feat(4,n,i) = length(a)+length(b);
     
     % Waveform length
     wavl = 0;
     current = 0;
     for p = 1:signal-1
-        current = abs(data_crouch(p+1,n,i) - data_crouch(p,n,i));
+        current = abs(data_out(p+1,n,i) - data_out(p,n,i));
         wavl = wavl+current;
     end
-    data_out(5,n,i) = wavl;
+    data_feat(5,n,i) = wavl;
     
     end
 end
 
 %% section 3: save data as .mat
 
+if participant == 1
+    if strcmp(activity,'crouch_')
+        crouch_feat1 = data_feat;
+        save('crouch_featurized1.mat','crouch_feat1');
+    elseif strcmp(activity,'slowWalk_')  
+        slowWalk_feat1 = data_feat;
+        save('slowWalk_featurized1.mat','slowWalk_feat1');
+    elseif strcmp(activity,'sitting_')
+        sitting_feat1 = data_feat;
+        save('sitting_featurized1.mat','sitting_feat1');
+    elseif strcmp(activity,'fastWalk_')
+        fastWalk_feat1 = data_feat;
+        save('fastWalk_featurized1.mat','fastWalk_feat1');
+    elseif strcmp(activity,'standing_')
+        standing_feat1 = data_feat;
+        save('standing_featurized1.mat','standing_feat1');
+    end
+elseif participant == 2
+    if strcmp(activity,'crouch_')
+        crouch_feat2 = data_feat;
+        save('crouch_featurized2.mat','crouch_feat2');
+    elseif strcmp(activity,'slowWalk_')  
+        slowWalk_feat2 = data_feat;
+        save('slowWalk_featurized2.mat','slowWalk_feat2');
+    elseif strcmp(activity,'sitting_')
+        sitting_feat2 = data_feat;
+        save('sitting_featurized2.mat','sitting_feat2');
+    elseif strcmp(activity,'fastWalk_')
+        fastWalk_feat2 = data_feat;
+        save('fastWalk_featurized2.mat','fastWalk_feat2');
+    elseif strcmp(activity,'standing_')
+        standing_feat2 = data_feat;
+        save('standing_featurized2.mat','standing_feat2');
+    end
+end
+    
+toc
