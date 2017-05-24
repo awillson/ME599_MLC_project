2993598bab82a9dee4cccc9ad026ebd351a56032
ajanders <ajanders@uw.edu>
2017-05-24 14:01:27 -0700

    made a PCA code

---
+++ PCA_Example.m
@@ -0,0 +1,36 @@
+%% PCA example from Brunton's book
+
+clear; close all; clc;
+
+%% Load data
+
+load ovariancancer;
+
+%% Do SVD
+
+[U, S, V] = svd(obs,'econ');
+
+%% Plot stuff
+
+figure;
+subplot(1,2,1)
+semilogy(diag(S),'k-o','LineWidth',1.5)
+set(gca,'FontSize',13), axis tight, grid on
+subplot(1,2,2)
+plot(cumsum(diag(S))./sum(diag(S)),'k-o','LineWidth',1.5)
+set(gca,'FontSize',13), axis tight, grid on
+set(gcf,'Position',[100 100 600 250])
+
+figure, hold on
+for i=1:size(obs,1)
+x = V(:,1)'*obs(i,:)';
+y = V(:,2)'*obs(i,:)';
+z = V(:,3)'*obs(i,:)';
+if(grp{i}=='Cancer')
+plot3(x,y,z,'rx','LineWidth',2);
+else
+plot3(x,y,z,'bo','LineWidth',2);
+end
+end
+view(85,25), grid on, set(gca,'FontSize',13)
+
--- reduce_PCA.m
+++ reduce_PCA.m
@@ -77,7 +77,7 @@ wavlData = testingData(:,5:5:end);
 
 %% SVD all of the things
 
-[u1, s1, v1] = svd(meanData);
+[u1, s1, v1] = svd(meanData,'econ');
 [u2, s2, v2] = svd(zCrossData);
 [u3, s3, v3] = svd(varData);
 [u4, s4, v4] = svd(sChangeData);
@@ -90,7 +90,7 @@ zCrossTotPow = 0;
 varTotPow = 0;
 sChangeTotPow = 0;
 wavlTotPow = 0;
-for i = 1:16
+parfor i = 1:16
     meanPCs(i) = s1(i,i);
     meanTotPow = meanTotPow + meanPCs(i);
     zCrossPCs(i) = s2(i,i);
@@ -122,5 +122,63 @@ xlabel('Number of Basis Vectors')
 ylabel('Cumulative Sum Over Total Sum of Principal Components')
 axis([0 16 0 1])
 
+%%
 
+figure, hold on
+for i=1:size(meanData,1)
+x = v4(:,1)'*sChangeData(i,:)';
+y = v4(:,2)'*sChangeData(i,:)';
+z = v4(:,3)'*sChangeData(i,:)';
+    if strcmp(trainingLabels(i),'crouch')
+        plot3(x,y,z,'rx','LineWidth',2);
+    elseif strcmp(trainingLabels(i),'fastWalk')
+        plot3(x,y,z,'bo','LineWidth',2);
+    elseif strcmp(trainingLabels(i),'sitting')
+        plot3(x,y,z,'go','LineWidth',2);
+    elseif strcmp(trainingLabels(i),'standing')
+        plot3(x,y,z,'yo','LineWidth',2);
+    elseif strcmp(trainingLabels(i),'slowWalk')
+        plot3(x,y,z,'mo','LineWidth',2);
+    end
+end
+view(85,25), grid on, set(gca,'FontSize',13)
 
+% % PCA example from Brunton's book
+% 
+% 
+% % Load data
+% 
+% load ovariancancer;
+% 
+% % Do SVD
+% 
+% [U, S, V] = svd(obs,'econ');
+% 
+% % Plot stuff
+% 
+% figure;
+% subplot(1,2,1)
+% semilogy(diag(S),'k-o','LineWidth',1.5)
+% set(gca,'FontSize',13), axis tight, grid on
+% subplot(1,2,2)
+% plot(cumsum(diag(S))./sum(diag(S)),'k-o','LineWidth',1.5)
+% set(gca,'FontSize',13), axis tight, grid on
+% set(gcf,'Position',[100 100 600 250])
+% 
+% figure, hold on
+% for i=1:size(obs,1)
+% x = V(:,1)'*obs(i,:)';
+% y = V(:,2)'*obs(i,:)';
+% z = V(:,3)'*obs(i,:)';
+% if(grp{i}=='Cancer')
+% plot3(x,y,z,'rx','LineWidth',2);
+% else
+% plot3(x,y,z,'bo','LineWidth',2);
+% end
+% end
+% view(85,25), grid on, set(gca,'FontSize',13)
+% 
+% 
+% 
+% 
+% 
