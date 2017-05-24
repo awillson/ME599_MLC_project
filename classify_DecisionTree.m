2887deef047838487e6985fabc0d30a0c134d642
ajanders <ajanders@uw.edu>
2017-05-23 21:33:23 -0700

    un-transposed reshaped data so that sensors were grouped in training and testing matrices

--- classify_DecisionTree.m
+++ classify_DecisionTree.m
@@ -12,27 +12,27 @@ load standing_featurized1.mat
 %% Reshape the training data into the same format as in the example code
 
 for i = 1:1200
-    trainingData(i,:) = reshape(crouch_feat1(:,:,i)', 1, 80);
+    trainingData(i,:) = reshape(crouch_feat1(:,:,i), 1, 80);
     trainingLabels{i} = 'crouch';
 end
 
 for i = 1:1200
-    trainingData(i+1200,:) = reshape(fastWalk_feat1(:,:,i)', 1, 80);
+    trainingData(i+1200,:) = reshape(fastWalk_feat1(:,:,i), 1, 80);
     trainingLabels{i+1200} = 'fastWalk';
 end
 
 for i = 1:1200
-    trainingData(i+2400,:) = reshape(sitting_feat1(:,:,i)', 1, 80);
+    trainingData(i+2400,:) = reshape(sitting_feat1(:,:,i), 1, 80);
     trainingLabels{i+2400} = 'sitting';
 end
 
 for i = 1:1200
-    trainingData(i+3600,:) = reshape(slowWalk_feat1(:,:,i)', 1, 80);
+    trainingData(i+3600,:) = reshape(slowWalk_feat1(:,:,i), 1, 80);
     trainingLabels{i+3600} = 'slowWalk';
 end
 
 for i = 1:1200
-    trainingData(i+4800,:) = reshape(standing_feat1(:,:,i)', 1, 80);
+    trainingData(i+4800,:) = reshape(standing_feat1(:,:,i), 1, 80);
     trainingLabels{i+4800} = 'standing';
 end
 
@@ -41,27 +41,27 @@ trainingLabels = trainingLabels';
 %% Reshape the testing data 
 
 for i = 1:1200
-    testingData(i,:) = reshape(crouch_feat1(:,:,i+1200)', 1, 80);
+    testingData(i,:) = reshape(crouch_feat1(:,:,i+1200), 1, 80);
     testingLabels{i} = 'crouch';
 end
 
 for i = 1:1200
-    testingData(i+1200,:) = reshape(fastWalk_feat1(:,:,i+1200)', 1, 80);
+    testingData(i+1200,:) = reshape(fastWalk_feat1(:,:,i+1200), 1, 80);
     testingLabels{i+1200} = 'fastWalk';
 end
 
 for i = 1:1200
-    testingData(i+2400,:) = reshape(sitting_feat1(:,:,i+1200)', 1, 80);
+    testingData(i+2400,:) = reshape(sitting_feat1(:,:,i+1200), 1, 80);
     testingLabels{i+2400} = 'sitting';
 end
 
 for i = 1:1200
-    testingData(i+3600,:) = reshape(slowWalk_feat1(:,:,i+1200)', 1, 80);
+    testingData(i+3600,:) = reshape(slowWalk_feat1(:,:,i+1200), 1, 80);
     testingLabels{i+3600} = 'slowWalk';
 end
 
 for i = 1:1200
-    testingData(i+4800,:) = reshape(standing_feat1(:,:,i+1200)', 1, 80);
+    testingData(i+4800,:) = reshape(standing_feat1(:,:,i+1200), 1, 80);
     testingLabels{i+4800} = 'standing';
 end
 
