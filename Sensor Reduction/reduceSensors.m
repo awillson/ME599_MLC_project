3a20c5d50235c35561e16c76b87e8a4f9480e1a7
ajanders <ajanders@uw.edu>
2017-05-30 21:32:45 -0700

    used normalized LDA script to reduced sensors

--- Sensor Reduction/reduceSensors.m
+++
@@ -1,7 +0,0 @@
-%%  Use this script to reduce sensors in a manner of average intelligence
-%   I'm sure there's a 'smart' way to do this, and the 'dumb' was is to do
-%   it combinatorially, but this should work to some degree.
-
-clear; close all; clc;
-
-%% 
